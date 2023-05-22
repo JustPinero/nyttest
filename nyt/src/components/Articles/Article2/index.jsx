c49f3b800bfe4e086b1cc01ef36b16540b1b69ff
Justin Pinero <justinpinero@gmail.com>
2023-05-22 01:09:31 -0400

    completed initial styling for components article1-article 3

--- nyt/src/components/Articles/Article2/index.jsx
+++ nyt/src/components/Articles/Article2/index.jsx
@@ -9,15 +9,16 @@ import {Article2Copy} from "./Copy"
 //IMAGES
 import Sneaker from "../../../assets/photos/Article2/AirJordan1OGBred1985_ChrisKarges1000px Chris Karges 1.png";
 
-//Non-Mobile Splash Page
 const Article2 = ()=>{
     return (
         <div className='article-container'>
-            <div>
-                <img src={Sneaker}/>
-            </div>
-            <div>
-                <ArticleLink articleData={Article2Copy} />
+            <div className="article-section">
+                <div className="sneaker-container">
+                    <img className="sneaker-image" alt="air jordan sneaker" src={Sneaker}/>
+                </div>
+                <div className="article-text_container">
+                    <ArticleLink articleData={Article2Copy} />
+                </div>
             </div>
         </div>
     )
--- nyt/src/components/Articles/Article3/index.jsx
+++ nyt/src/components/Articles/Article3/index.jsx
@@ -7,18 +7,26 @@ import "./styles.css";
 /* COPY */
 import {Article3Copy} from "./Copy"
 /* IMAGES */
-import Ball from "../../../assets/photos/Article3/mokrimidnightbball110 2.png";
-import Blur from "../../../assets/photos/Article3/image 596.png";
+import Blur from "../../../assets/photos/Article3/mokrimidnightbball110 2.png";
+import Ball from "../../../assets/photos/Article3/image 596.png";
 
 const Article3 = ()=>{
     return (
         <div className='article-container'>
-            <div>
-                <img src={Blur}/>
+            <div className="article-section">
+                <div className="blur-container">
+                    <img className="blur-image" src={Blur} alt="basketball players in a game blurred for the effect of speed" />
+                </div>
             </div>
-            <div>
-                <ArticleLink articleData={Article3Copy} />
-                <img src={Ball}/>
+            <div className="article-section">
+                <div className="article-row">
+                    <div className="link-container">
+                        <ArticleLink articleData={Article3Copy} />
+                    </div>
+                    <div className="ball-container">
+                        <img className="ball-image" alt="player dribbling basketball" src={Ball}/>
+                    </div>
+                </div>
             </div>
         </div>
     )
